fe3198fe5373454decb75d53fb46ed6343ebd07c
Mark Rasavong <mark.rasavong@gmail.com>
2022-02-27 10:26:16 +0100

    edit server files for heroku deplyoment v2

--- server/db.js
+++ server/db.js
@@ -5,8 +5,14 @@ const { Pool } = pg;
 
 dotenv.config();
 
+const isProduction = process.env.NODE_ENV === "production";
+const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+const proConfig = process.env.DATABASE_URL; //heroku addons
+
 const pool = new Pool({
-  connectionString: `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
+  connectionString:
+    isProduction ? proConfig : devConfig,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
 export default pool;
--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 //ROUTES
 app.use('/api/auth', authRoutes);
@@ -20,6 +20,6 @@ app.get("*", (req, res) => {
   res.json({ message: "Please redirect to the appropriate route." })
 })
 
-app.listen(PORT, function () {
-  console.log(`Server connected @ ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server has started on port ${PORT}`);
 });

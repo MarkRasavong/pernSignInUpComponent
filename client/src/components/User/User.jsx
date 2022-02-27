9062ab24769218622a491743a7561a47dafe015d
Mark Rasavong <mark.rasavong@gmail.com>
2022-02-27 11:22:45 +0100

    edit user component

--- client/src/components/User/User.jsx
+++ client/src/components/User/User.jsx
@@ -7,7 +7,6 @@ import EditModal from '../EditModal/EditModal';
 
 const User = () => {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
-  const { first_name, last_name, id, autoritzacio, email } = user?.data;
   const dispatch = useDispatch();
   let navigate = useNavigate();
   const isAdmin = process.env.REACT_APP_CODIGO_ADMIN;
@@ -25,16 +24,16 @@ const User = () => {
       <Container>
         <Grid container="true" spacing={2} direction="column" justifyContent="center" alignItems="center" sx={{ marginTop: '30vh' }}>
           <Grid xs={12}>
-            <Typography>Hello {first_name} {last_name},</Typography>
+            <Typography>Hello {user.first_name} {user.last_name},</Typography>
           </Grid>
           <Grid xs={12}>
-            <Typography>Your ID is: { id }</Typography>
+            <Typography>Your ID is: { user.id }</Typography>
           </Grid>
           <Grid xs={12} sx={{ marginTop: '10px' }}>
             { userAuthStatus === isAdmin &&
               <Button sx={{marginRight: '10px' }} variant="outlined" color="warning" onClick={() => navigate('/admin')}>Admin Dash</Button>
             }
-            <EditModal userId={id} fName={first_name} lName={last_name} auth={autoritzacio} email={email}/>
+            <EditModal userId={user.id} fName={user.first_name} lName={user.last_name} auth={user.autoritzacio} email={user.email}/>
             <Button variant="outlined" color="secondary" onClick={logoutClick}>Sign Out</Button>
           </Grid>
         </Grid>

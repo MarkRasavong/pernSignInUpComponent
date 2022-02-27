80c1ff9d55a8932e79df13a90021923c93ada34c
Mark Rasavong <mark.rasavong@gmail.com>
2022-02-27 09:51:11 +0100

    edit apis for heroku deploy

--- client/src/apis/PortalApi.js
+++ client/src/apis/PortalApi.js
@@ -2,7 +2,10 @@ import axios from "axios";
 import axiosRetry from "axios-retry";
 
 const PortalApi = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    process.env.NODE_ENV !== 'production'
+      ? "http://localhost:5000/api"
+      : "https://thawing-sea-19132.herokuapp.com/api",
   timeout: 5000,
 });
 
